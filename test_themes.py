99d63d64ffb64dddfd65d02b4a30c651c07d08f6
Goodluck <goodluck.nwokorie@madetech.com>
2025-11-24 12:51:50 +0000

    i added text and fixed the problem i created so it fine

--- test_themes.py
+++ test_themes.py
@@ -1,4 +1,10 @@
-from themes import x2
-def ():
-    #assert len(x2)>10
-    assert True is True
+from themes import duties
+
+def test_has_13_duties():
+    assert len(duties) == 13
+
+def test_first_duty_starts_with_duty_1():
+    assert duties[0].startswith("Duty 1")
+
+def test_last_duty_is_duty_13():
+    assert duties[-1].startswith("Duty 13")
