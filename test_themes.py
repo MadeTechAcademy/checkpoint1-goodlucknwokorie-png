80eb3676988eab7ef42520e8deae841dae6e9093
Goodluck <goodluck.nwokorie@madetech.com>
2025-11-24 13:42:00 +0000

    added new test so

--- test_themes.py
+++ test_themes.py
@@ -8,4 +8,4 @@ def test_first_duty_starts_with_duty_1():
     assert duties[0].startswith("Duty 1")
 
 def test_last_duty_is_duty_13():
-    assert duties[-1].startswith("Duty 13")
+    assert duties[-1].startswith("Duty 13") 
